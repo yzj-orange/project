6f1bc34ab9c844d58bcdf14f317fd0f3d005a297
yzj-orange <2871798874@qq.com>
2020-10-26 15:42:50 +0800

    新加入gateway网关 ，仅为模拟，需要设置yml 转发ip

---
+++ eureka/spring-gate-way/src/main/java/com/example/springgateway/SpringGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGateWayApplication.class, args);
+    }
+
+}

1dabef5cd0ed841e07fbdad911889462125f3199
yzj-orange <2871798874@qq.com>
2021-06-05 22:13:20 +0800

    更新

--- vuedemo1/src/components/HelloWorld.vue
+++ vuedemo1/src/components/HelloWorld.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1 class="home">this is home page</h1>
+  
+  <div>
+    <h1 class="home">this is home page</h1>
+    <button @click=";
+    if(testData1==1){
+      testData1=2
+    }else if(testData1==2){
+      testData1=1
+      }">TEST{{testData1}}</button>
+    {{testData1}}
+  </div>
 </template>
 
 <style>
@@ -7,3 +17,12 @@
   color: white;
 }
 </style>
+<script>
+export default {
+  data() {
+    return {
+      testData1:1
+    }
+  },
+}
+</script>
